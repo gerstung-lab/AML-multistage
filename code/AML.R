3ffc087b03862584dfec85560d1ce51211645f10
mg14 <mg14@sanger.ac.uk>
2014-03-01 12:16:34 +0000

    -n AML data 0

--- code/AML.R
+++ code/AML.R
@@ -1,12 +1,12 @@
-# TODO: Add comment
-# 
-# Author: mg14
-###############################################################################
-
+#' AML data analysis
+#' =================
+#' Load the mutation data
+#' ----------------------
 mutationData = read.table("data/AML2_prelim_subs_for_peter2.txt", sep="\t", header=FALSE)
 oncogenics <- (table(mutationData[,c(3,19)]) > 0)+0
 rownames(oncogenics) <- sub("WGA_","",rownames(oncogenics))
 
+#' Compute gene-gene interactions
 interactions <- sapply(1:ncol(oncogenics), function(i) sapply(1:ncol(oncogenics), function(j) {f<- try(fisher.test(oncogenics[,i], oncogenics[,j]), silent=TRUE); if(class(f)=="try-error") 0 else ifelse(f$estimate>1, -log10(f$p.val),log10(f$p.val))} ))
 odds <- sapply(1:ncol(oncogenics), function(i) sapply(1:ncol(oncogenics), function(j) {f<- try(fisher.test(oncogenics[,i] + .5, oncogenics[,j] +.5), silent=TRUE); if(class(f)=="try-error") f=NA else f$estimate} ))
 diag(interactions) <- 0
@@ -17,7 +17,9 @@ odds[odds<1e-3] = 1e-4
 odds[odds>1e3] = 1e4
 logodds=log10(odds)
 
-pdf(paste(Sys.Date(),"-Interactions.pdf", sep=""), 4,4, pointsize = 8) ## HEATMAP OF ALL
+#' Plot
+#+ dev=c("png","pdf"), fig.width=4, fig.height=4, fig.args=list( pointsize = 8)
+#pdf(paste(Sys.Date(),"-Interactions.pdf", sep=""), 4,4, pointsize = 8) ## HEATMAP OF ALL
 par(bty="n", mgp = c(2,.5,0), mar=c(3,3,2,2)+.1, las=2, tcl=-.33)
 ix = TRUE#colnames(interactions) %in% colnames(all_genotypes)
 h = hclust(dist(interactions[ix,ix]))
@@ -44,8 +46,11 @@ axis(side = 4, at = seq(1,7) + .5, cex.axis=.5, tcl=-.15, label=10^seq(-3,3), la
 points(x=rep(ncol(interactions),2)+2.5, y=10:11, pch=c("*","."))
 image(x=rep(ncol(interactions),2)+c(2,3), y=(12:13) -0.5, z=matrix(1), col=brewer.pal(3,"BrBG"), add=TRUE)
 mtext(side=4, at=12:10, c("P > 0.05", "BH < 0.1", "Bf. < 0.05"), cex=.5, line=0.2)
-dev.off()
+#dev.off()
 
+#' Clinical data
+#' -------------
+#' #### Loading
 clinicalData <- read.table("data/AML.clin.prelim4.txt", sep="\t", header=TRUE, na.strings = "na")
 clinicalData$BM.blasts <- as.numeric(as.character(clinicalData$BM.blasts))
 
@@ -53,50 +58,59 @@ library(survival)
 surv <- Surv(clinicalData$efs, clinicalData$efsstat)
 
 m <- match(rownames(oncogenics), clinicalData$PDID)
-source("~/Projects/hdcox/hdcox/R/ecoxph.R")
+source("~/Projects/CoxHD/CoxHD/R/ecoxph.R")
 
-
+#' #### A few functions
 makeInteractions <- function(X,Y){
 	do.call(cbind, lapply(X, `*`, Y))
 }
 
 makeInteger <- function(F){
-	res <- do.call(cbind,lapply(levels(F), `==`, F))
+	res <- as.data.frame(lapply(levels(F), `==`, F))
 	colnames(res) <- levels(F)
 	res
 }
 
+#' #### Basic KM plot
 library(RColorBrewer)
 set1 <- brewer.pal(9, "Set1")
-plot(survfit(surv[m] ~ TP53 + TPL, data=X), col=rep(set1[1:2],each=2), lty=1:2, mark=16)
+plot(survfit(surv[m] ~ TP53 + TPL, data=dataList), col=rep(set1[1:2],each=2), lty=1:2, mark=16)
 
+#' ### Anova with CoxRFX
+#' Prepating data
+dataList <-list(G = data.frame(oncogenics), 
+		T = cbind(makeInteger(clinicalData$Study[m]), TPL=clinicalData$TPL[m]),
+		C = cbind(clinicalData[m, c("Age", "gender","BM.blasts","wbc","LDH_")], makeInteger(clinicalData$TypeAML[m])[,-1]))
+dataList$GG <- makeInteractions(data.frame(oncogenics), data.frame(oncogenics))
+dataList$GG <- dataList$GG[,-seq(1,ncol(dataList$GG), ncol(oncogenics)+1)] 
+dataList$GG <- dataList$GG[,colSums(dataList$GG)>0] 
+dataList$GT <- makeInteractions(dataList$G, dataList$T)
+dataList$GT <- dataList$GT[,colSums(dataList$GT, na.rm=TRUE) > 0]
 
-X <- list(G = data.frame(oncogenics), 
-		T = cbind(makeInteger(clinicalData$Study[m]), clinicalData$TPL[m]),
-		C = clinicalData[m, c("Age", "gender","BM.blasts","wbc","LDH_")])
-X$GG <- makeInteractions(data.frame(oncogenics), data.frame(oncogenics))
-X$GG <- X$GG[,-seq(1,ncol(X$GG), ncol(oncogenics)+1)] 
-X$GG <- X$GG[,colSums(X$GG)>0] 
-X$GT <- makeInteractions(X$G, X$T)
-X$GT <- X$GT[,colSums(X$GT, na.rm=TRUE) > 0]
+dataFrame <- do.call(cbind,dataList)
 
+#' Fit model
+#+ cache=TRUE
+c <- coxRFX(dataFrame, surv[m], groups=unlist(sapply(names(dataList), function(x) rep(x, ncol(dataList[[x]])))))
 
-c <- coxRFX(do.call(cbind,X), surv[m], groups=unlist(sapply(names(X), function(x) rep(x, ncol(X[[x]])))))
-
+#' A few performance measures
 library(Hmisc)
 rcorr.cens(-rowSums(partialRisk(c)), surv[m])
 risk <- rowSums(partialRisk(c))
 AUC.uno(surv[m][!is.na(risk)], surv[m][!is.na(risk)], risk[!is.na(risk)], 5000)
 
+#' Risk terciles
 plot(survfit(surv[m][!is.na(risk)] ~ cut(risk[!is.na(risk)], 5)), col=rep(set1[1:2],each=2), lty=1:2, mark=16)
 
+#' Partical values of Harrel's C
 barplot(partialC(c))
 
+#' Holding out a test set.
 set.seed(42)
 testIx <- sample(c(TRUE,FALSE), length(m), replace=TRUE, prob=c(0.66,0.34))
-d <- coxRFX(do.call(cbind,X)[testIx,], surv[m][testIx], groups=unlist(sapply(names(X), function(x) rep(x, ncol(X[[x]])))))
+d <- coxRFX(dataFrame[testIx,], surv[m][testIx], groups=unlist(sapply(names(dataList), function(x) rep(x, ncol(dataList[[x]])))))
 
-p <- as.matrix(do.call(cbind,X)[!testIx,]) %*% d$coefficients
+p <- as.matrix(dataFrame[!testIx,]) %*% d$coefficients
 rcorr.cens(- na.omit(p), na.omit(surv[m][!testIx]))
 AUC.uno(na.omit(surv[m]), na.omit(surv[m][!testIx]), na.omit(p), 1000)
 
