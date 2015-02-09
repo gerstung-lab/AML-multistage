5625918d487ae96fe355b363d79b54b2ff7466a5
mg14 <mg14@sanger.ac.uk>
2015-02-09 10:04:30 +0000

    Old, working version

--- code/relapse/ui.R
+++ code/relapse/ui.R
@@ -7,7 +7,7 @@
 library(shiny)
 library(CoxHD)
 load("predict.RData")
-pdid <- "other"
+
 
 # Define UI for application that plots random distributions 
 shinyUI(pageWithSidebar(
@@ -16,14 +16,11 @@ shinyUI(pageWithSidebar(
 				headerPanel("AML prediction"),
 				
 				# Sidebar with a slider input for number of observations
-				do.call("sidebarPanel", 
-						c(selectInput("pdid", "Select sample", c("other",rownames(coxRFXCirTD$Z)), selected = "1", multiple=FALSE),
-								lapply(c("transplantRel",names(sort(apply(coxRFXCirTD$Z,2,var)*coef(coxRFXCirTD)^2, decreasing=TRUE)[-102])), function(x) numericInput(x, paste(x, " (mean=",round(mean(coxRFXCirTD$Z[,x]), 2),"; HR_CIR=",round(exp(coef(coxRFXCirTD)[x]),2),"; HR_NRM=",round(exp(coef(coxRFXNrmTD)[x]),2),"; HR_PRM=",round(exp(coef(coxRFXPrsTD)[x]),2),")", sep=""), ifelse(pdid=="other",NA,coxRFXCirTD$Z[pdid,x]), )
-								))
+				do.call("sidebarPanel", lapply(c("transplantRel",names(sort(apply(coxRFXCirTD$Z,2,var)*coef(coxRFXCirTD)^2, decreasing=TRUE)[-102])), function(x) numericInput(x, paste(x, " (mean=",round(mean(coxRFXCirTD$Z[,x]), 3),"; HR=",round(exp(coef(coxRFXCirTD)[x]),3),")", sep=""), NA, ))
 				),
 				# Show a plot of the generated distribution
 				mainPanel(
-						plotOutput(outputId="KM",height="600px"),
+						plotOutput(outputId="KM",height="700px"),
 						tableOutput(outputId = "Risk"),
 						tableOutput(outputId = "Tab")
 				)
