c87ff52c1ba150b8618f91f21cd36c81ec170ea8
mg14 <mg14@sanger.ac.uk>
2015-03-16 19:52:11 +0000

    Updated code for farmulations n=100,200,500,...,10000

--- code/Farmulations2.R
+++ code/Farmulations2.R
@@ -1,8 +1,8 @@
-load("sim2Data.RData")
-source("../../mg14/R/mg14.R")
+load("simData2.RData")
+library(mg14)
 library(CoxHD)
 library(parallel)
-nData <- c(100, 1000, 10000)
+nData <- c(100, 200, 500, 1000, 2000, 5000, 10000)
 nJobs <- as.numeric(Sys.getenv("NJOBS"))
 
 jobIndex <- as.numeric(Sys.getenv("LSB_JOBINDEX"))
@@ -13,16 +13,13 @@ simCoef <- CoxHD:::SimCoef(coxRFXFitOsTDGGc, groups = simGroups)
 simRisk <- as.matrix(simDataFrame[names(whichRFXOsTDGG)]) %*% simCoef[names(whichRFXOsTDGG)]
 simSurv <- SimSurvNonp(simRisk, os)
 
-w100 <- sample(1:nrow(simDataFrame), 100)
-fit100 <- CoxRFX(simDataFrame[w100,names(whichRFXOsTDGG)], simSurv[w100], simGroups[names(whichRFXOsTDGG)], nu=1,which.mu=mainGroups)
-fit100$X <- NULL
-
-w1000 <- sample(1:nrow(simDataFrame), 1000)
-fit1000 <- CoxRFX(simDataFrame[w1000,names(whichRFXOsTDGG)], simSurv[w1000], simGroups[names(whichRFXOsTDGG)],which.mu=mainGroups)
-fit1000$X <- NULL
-
-fit10000 <- CoxRFX(simDataFrame[names(whichRFXOsTDGG)], simSurv, simGroups[names(whichRFXOsTDGG)],which.mu=mainGroups)
-fit10000$X <- NULL
+for(n in nData){
+	s <- if(n < 10000) sample(1:nrow(simDataFrame), n) else 1:10000
+	f <- CoxRFX(simDataFrame[s,names(whichRFXOsTDGG)], simSurv[s], simGroups[names(whichRFXOsTDGG)], nu=1,which.mu=mainGroups)
+	f$X <- NULL
+	assign(paste0("w",n), s)
+	assign(paste0("fit",n),f)	
+}
 
 rm(simDataFrame)
 
