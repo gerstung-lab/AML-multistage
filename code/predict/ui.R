d96b99eafe0167506dd798f2585e0427ae2f5f0d
mg14 <mg14@sanger.ac.uk>
2015-03-26 16:45:19 +0000

    Predicting remission and early deaths

--- code/predict/ui.R
+++ code/predict/ui.R
@@ -13,32 +13,33 @@ load("predictGG.RData", envir=globalenv())
 shinyUI(fluidPage(
 				
 				# Application title
-			    titlePanel("AML prediction tool"),
+				titlePanel("AML prediction tool"),
 				
 				fluidRow(
-							# Sidebar with a slider input for number of observations
-							column(3, 
-									wellPanel(
-												selectInput("pdid", "Select sample", c("reset",rownames(data)), selected = "reset", multiple=FALSE) ## select sample
-											),
-											wellPanel(
-													checkboxGroupInput("ciType", "Confidence intervals:", c("analytical","simulated"), selected = "analytical") ## CI type
-											),
-											wellPanel(
-												uiOutput("ui")
-											)
-							),
-							
-							# Show a plot of the generated distribution
-							column(8,
-									plotOutput(outputId="KM",height="600px"),
-									tabsetPanel(
-											tabPanel('Risk',
-													dataTableOutput("Risk")),
-											tabPanel("Coefficients",
-													dataTableOutput("Tab"))
-									))
-
-						)
+						# Sidebar with a slider input for number of observations
+						column(3, 
+								wellPanel(
+										selectInput("pdid", "Select sample", c("reset",rownames(data)), selected = "reset", multiple=FALSE), ## select sample
+										submitButton("Submit")
+								),
+								wellPanel(
+										checkboxGroupInput("ciType", "Confidence intervals:", c("analytical","simulated"), selected = "analytical") ## CI type
+								),
+								wellPanel(
+										uiOutput("ui")
+								)
+						),
+						
+						# Show a plot of the generated distribution
+						column(8,
+								plotOutput(outputId="KM",height="800px"),
+								tabsetPanel(
+										tabPanel('Risk',
+												dataTableOutput("Risk")),
+										tabPanel("Coefficients",
+												dataTableOutput("Tab"))
+								))
+				
+				)
 		))
 
