7310b1c2bc7075c0fa63fe7c4dc56452a3fc5ecc
mg14 <mg14@sanger.ac.uk>
2015-03-27 11:18:48 +0000

    Cherry-picked ui.R from ba062a3e4e0cddd1008c9ba4c0f31a052fc371df

--- code/predict/ui.R
+++ code/predict/ui.R
@@ -6,39 +6,42 @@
 
 library(shiny)
 library(CoxHD)
-load("predictGG.RData", envir=globalenv())
+#load("predictGG.RData", envir=globalenv())
 #data <- coxRFXCirTD$Z
 
 # Define UI for application that plots random distributions 
 shinyUI(fluidPage(
 				
 				# Application title
-			    titlePanel("AML prediction tool"),
+				titlePanel("AML prediction tool"),
 				
 				fluidRow(
-							# Sidebar with a slider input for number of observations
-							column(3, 
-									wellPanel(
-												selectInput("pdid", "Select sample", c("reset",rownames(data)), selected = "reset", multiple=FALSE) ## select sample
-											),
-											wellPanel(
-													checkboxGroupInput("ciType", "Confidence intervals:", c("analytical","simulated"), selected = "analytical") ## CI type
-											),
-											wellPanel(
-												uiOutput("ui")
-											)
-							),
-							
-							# Show a plot of the generated distribution
-							column(8,
-									plotOutput(outputId="KM",height="600px"),
-									tabsetPanel(
-											tabPanel('Risk',
-													dataTableOutput("Risk")),
-											tabPanel("Coefficients",
-													dataTableOutput("Tab"))
-									))
-
-						)
+						# Sidebar with a slider input for number of observations
+						column(3, 
+								wellPanel(
+										selectInput("pdid", tags$b("Select sample"), c("reset",rownames(data)), selected = "reset", multiple=FALSE), ## select sample
+										submitButton("Load presets")
+								),
+								wellPanel(
+										tags$b("Prognostic variables"),
+										submitButton("Compute survival"),
+										uiOutput("ui")
+								),
+								wellPanel(
+										checkboxGroupInput("ciType", tags$b("Confidence intervals"), c("analytical","simulated"), selected = "analytical") ## CI type
+								)
+						),
+						
+						# Show a plot of the generated distribution
+						column(8,
+								plotOutput(outputId="KM",height="1200px"),
+								tabsetPanel(
+										tabPanel('Risk',
+												dataTableOutput("Risk")),
+										tabPanel("Coefficients",
+												dataTableOutput("Tab"))
+								))
+				
+				)
 		))
 
