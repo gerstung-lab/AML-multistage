a725eee942005dffd0110f8ed0908a62e0b457d9
Moritz Gerstung <mg14@sanger.ac.uk>
2015-12-15 09:17:18 +0000

    Clean up

--- code/Farmulations.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#BSUB -J Farmulations[1-1000]
-#BSUB -o logs/Farmulations.out%J
-#BSUB -e logs/Farmulations.err%J
-#BSUB -M 1000
-#BSUB -R "select[mem>1000] rusage[mem=1000]" 
-export NJOBS=1000
-R --no-save < Farmulations.R
